ebe7c4af3b2ae50d6e859f407e1bc3a2e48f4e17
wayha1 <vehaseng1122@gmail.com>
2025-06-22 22:28:31 +0700

    update

--- database/migrations/2025_06_07_155622_create_attendances_table.php
+++ database/migrations/2025_06_07_155622_create_attendances_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,14 @@ return new class extends Migration
     {
         Schema::create('attendances', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignIdFor(User::class);
+            $table->string('name');
+            $table->text('description')->nullable();
+            
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+
             $table->timestamps();
         });
     }
